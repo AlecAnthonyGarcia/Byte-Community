cd1f0b5eacd3d496ac43873c50750f72b52da266
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2020-01-27 17:58:26 -0800

    Add share button

--- client/src/ByteVideo/index.js
+++ client/src/ByteVideo/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import './style.scss';
 
 import { Link } from 'react-router-dom';
-import { Icon } from 'antd';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+import { Dropdown, Icon, Menu, message } from 'antd';
 import moment from 'moment';
 
 import UserAvatar from '../UserAvatar';
@@ -100,13 +101,77 @@ class ByteVideo extends React.Component {
 		};
 
 		const ShareButton = () => {
+			const baseUrl = window.location.origin;
+			const postUrl = `${baseUrl}/post/${postId}`;
+			const socialShareWindowOptions =
+				'toolbar=0,status=0,resizable=1,width=626,height=436';
+
+			const handleMenuClick = ({ key }) => {
+				switch (key) {
+					case 'open':
+						window.open(postUrl, '_blank');
+						break;
+					case 'download':
+						window.open(videoSrc, '_blank');
+						break;
+					case 'facebook':
+						shareToFacebook();
+						break;
+					case 'twitter':
+						shareToTwitter();
+						break;
+					default:
+				}
+			};
+
+			const shareToFacebook = () => {
+				const url = `https://facebook.com/sharer.php?display=popup&u=${postUrl}`;
+				window.open(url, 'sharer', socialShareWindowOptions);
+			};
+
+			const shareToTwitter = () => {
+				const shareText = `Check out this byte by ${username} on the byte community:`;
+
+				const url = `http://twitter.com/intent/tweet?text=${shareText}&url=${postUrl}`;
+
+				window.open(url, 'sharer', socialShareWindowOptions);
+			};
+
+			const onLinkCopied = () => {
+				message.success('Post link has been copied.');
+			};
+
+			const shareMenu = (
+				<Menu onClick={handleMenuClick}>
+					<Menu.Item key="open">
+						<Icon type="link" /> Open link
+					</Menu.Item>
+					<Menu.Item key="copy">
+						<CopyToClipboard text={postUrl} onCopy={onLinkCopied}>
+							<span>
+								<Icon type="copy" /> Copy link
+							</span>
+						</CopyToClipboard>
+					</Menu.Item>
+					<Menu.Item key="download">
+						<Icon type="download" /> Download Video
+					</Menu.Item>
+					<Menu.Item key="facebook">
+						<Icon type="facebook" /> Share to Facebook
+					</Menu.Item>
+					<Menu.Item key="twitter">
+						<Icon type="twitter" /> Share to Twitter
+					</Menu.Item>
+				</Menu>
+			);
+
 			return (
-				<Link to={`/post/${postId}`}>
+				<Dropdown overlay={shareMenu} trigger={['click']}>
 					<div className="video-stat-icon-container">
 						<Icon type="share-alt" style={{ fontSize: '24px' }} />
 						<span>Share</span>
 					</div>
-				</Link>
+				</Dropdown>
 			);
 		};
 
