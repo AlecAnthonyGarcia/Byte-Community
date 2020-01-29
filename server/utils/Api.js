48802c815a3d9a550d0edd26bfec03cd2bd4deca
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2020-01-28 21:56:17 -0800

    Handle API errors

--- server/utils/Api.js
+++ server/utils/Api.js
@@ -11,61 +11,72 @@ const {
 axios.defaults.headers.common['Authorization'] = ''; // TODO: add your own authorization token
 
 async function getUser(userId) {
-	const response = await axios.get(`${ACCOUNT_API}id/${userId}`);
-	const { data } = response;
-	return data;
+	try {
+		const { data } = await axios.get(`${ACCOUNT_API}id/${userId}`);
+		return data;
+	} catch (err) {
+		return { data: {} };
+	}
 }
 
 async function searchUser(query) {
-	const response = await axios.get(`${ACCOUNT_API}prefix/${query}`);
-	const { data } = response;
-	return data;
+	try {
+		const { data } = await axios.get(`${ACCOUNT_API}prefix/${query}`);
+		const { error } = data;
+
+		if (error) {
+			return {
+				data: { accounts: [] }
+			};
+		} else {
+			return data;
+		}
+	} catch (err) {
+		return { data: { accounts: [] } };
+	}
 }
 
 async function getUserPosts(userId, cursor) {
-	let url = `${ACCOUNT_API}id/${userId}/posts`;
-	if (cursor) {
-		url = `${url}?cursor=${cursor}`;
-	}
-	const response = await axios.get(url);
-	const { data } = response;
-	return data;
+	return await getFeed(`${ACCOUNT_API}id/${userId}/posts`, cursor);
 }
 
 async function getUserRebytes(userId, cursor) {
-	let url = `${ACCOUNT_API}id/${userId}/rebytes`;
-	if (cursor) {
-		url = `${url}?cursor=${cursor}`;
-	}
-	const response = await axios.get(url);
-	const { data } = response;
-	return data;
+	return await getFeed(`${ACCOUNT_API}id/${userId}/rebytes`, cursor);
 }
 
 async function getPost(postId) {
-	const response = await axios.get(`${POST_API}id/${postId}`);
-	const { data } = response;
-	return data;
+	try {
+		const { data } = await axios.get(`${POST_API}id/${postId}`);
+		return data;
+	} catch (err) {
+		return { data: {} };
+	}
 }
 
 async function getPostComments(postId, cursor) {
-	let url = `${POST_API}id/${postId}/feedback/comment`;
-	if (cursor) {
-		url = `${url}?cursor=${cursor}`;
+	try {
+		let url = `${POST_API}id/${postId}/feedback/comment`;
+		if (cursor) {
+			url = `${url}?cursor=${cursor}`;
+		}
+		const { data } = await axios.get(url);
+		return data;
+	} catch (err) {
+		return { data: { comments: [] } };
 	}
-	const response = await axios.get(url);
-	const { data } = response;
-	return data;
 }
 
 async function getPostLikes(postId, cursor) {
-	let url = `${POST_API}id/${postId}/feedback/like`;
-	if (cursor) {
-		url = `${url}?cursor=${cursor}`;
+	try {
+		let url = `${POST_API}id/${postId}/feedback/like`;
+		if (cursor) {
+			url = `${url}?cursor=${cursor}`;
+		}
+		const { data } = await axios.get(url);
+		return data;
+	} catch (err) {
+		return { data: { accounts: [] } };
 	}
-	const response = await axios.get(url);
-	const { data } = response;
-	return data;
 }
 
 async function getPopularFeed(cursor) {
@@ -81,12 +92,15 @@ async function getLatestFeed(cursor) {
 }
 
 async function getFeed(url, cursor) {
-	if (cursor) {
-		url = `${url}?cursor=${cursor}`;
+	try {
+		if (cursor) {
+			url = `${url}?cursor=${cursor}`;
+		}
+		const { data } = await axios.get(url);
+		return data;
+	} catch (err) {
+		return { data: { posts: [] } };
 	}
-	const response = await axios.get(url);
-	const { data } = response;
-	return data;
 }
 
 async function getCategoryFeed(categoryName, sort, cursor) {
@@ -107,9 +121,12 @@ async function getCategoryFeed(categoryName, sort, cursor) {
 }
 
 async function getExploreCategories() {
-	const response = await axios.get(`${EXPLORE_API}`);
-	const { data } = response;
-	return data;
+	try {
+		const { data } = await axios.get(`${EXPLORE_API}`);
+		return data;
+	} catch (err) {
+		return { data: { layout: [] } };
+	}
 }
 
 const Api = {
