8867ade32977cf6bae177c776765c219886b8ca6
Alec Garcia <AlecAnthonyGarcia@gmail.com>
2020-01-31 00:05:20 -0800

    Add 3rd party notice to LoginModal

--- client/src/AuthModal/LoginModal/index.js
+++ client/src/AuthModal/LoginModal/index.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux';
 
 import { authenticate } from '../authActions';
 
-import { Modal, Form, Input, Button, Icon, Alert } from 'antd';
+import { Modal, Form, Input, Button, Icon, Alert, Typography } from 'antd';
+
+const { Title } = Typography;
 
 class LoginModal extends React.Component {
 	constructor(props) {
@@ -77,42 +79,76 @@ class LoginModal extends React.Component {
 						<Alert message={errorMessageText} type="error" />
 					)}
 
-					<p>Please paste the code that Google asked you to copy:</p>
+					<Alert
+						message={
+							<span>
+								Notice: We will securely sign you in to byte but please be aware
+								that we are a 3rd party website and aren't officially endorsed
+								by byte.
+								<br />
+								<br />
+								The code for this website is{' '}
+								<a
+									href="https://github.com/CaliAlec/Byte-Community"
+									target="_blank"
+								>
+									open source on GitHub
+								</a>{' '}
+								and you can trust us with your account, but please be careful
+								about giving your byte info to any 3rd parties.
+								<br />
+								<br />
+								You can view Dom's advice on the official byte forums{' '}
+								<a
+									href="https://community.byte.co/t/important-note-about-unofficial-websites/47684"
+									target="_blank"
+								>
+									here
+								</a>
+								.
+							</span>
+						}
+						type="warning"
+					/>
 
-					<Form.Item>
-						{getFieldDecorator('code', {
-							rules: [
-								{
-									required: true,
-									message: 'Code cannot be empty.'
-								},
-								{
-									pattern: new RegExp('^4/'),
-									message: 'Invalid code format.'
-								}
-							]
-						})(
-							<Input
-								prefix={
-									<Icon type="google" style={{ color: 'rgba(0,0,0,.25)' }} />
-								}
-								placeholder="Code"
+					<div style={{ marginTop: '15px' }}>
+						<h3>Please paste the code that Google asked you to copy:</h3>
+
+						<Form.Item>
+							{getFieldDecorator('code', {
+								rules: [
+									{
+										required: true,
+										message: 'Code cannot be empty.'
+									},
+									{
+										pattern: new RegExp('^4/'),
+										message: 'Invalid code format.'
+									}
+								]
+							})(
+								<Input
+									prefix={
+										<Icon type="google" style={{ color: 'rgba(0,0,0,.25)' }} />
+									}
+									placeholder="Code"
+									size="large"
+								/>
+							)}
+						</Form.Item>
+
+						<Form.Item>
+							<Button
+								block
 								size="large"
-							/>
-						)}
-					</Form.Item>
-
-					<Form.Item>
-						<Button
-							block
-							size="large"
-							type="primary"
-							htmlType="submit"
-							loading={loading}
-						>
-							Login
-						</Button>
-					</Form.Item>
+								type="primary"
+								htmlType="submit"
+								loading={loading}
+							>
+								Login
+							</Button>
+						</Form.Item>
+					</div>
 				</Form>
 			</Modal>
 		);
